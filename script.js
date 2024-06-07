38c0a5b23ebcd4df8cdd35e977cd6c84b13ee40f
kajal-2011 <kajalrekha888@gmail.com>
2024-06-07 21:36:43 +0600

    done

--- script.js
+++ script.js
@@ -76,6 +76,48 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   document.querySelector('[data-tab="all"]').click();
+
+
+  
+  function animateValue(id, start, end, duration) {
+    let obj = document.getElementById(id);
+    let range = end - start;
+    let current = start;
+    let increment = end > start ? 1 : -1;
+    let stepTime = Math.abs(Math.floor(duration / range));
+    let timer = setInterval(function () {
+      current += increment;
+      obj.textContent = current + "+";
+      if (current == end) {
+        clearInterval(timer);
+      }
+    }, stepTime);
+  }
+
+  animateValue("projectCount", 0, 12, 1000);
+  animateValue("memberCount", 0, 22, 1000);
+
+  function animateValue(id, start, end, duration) {
+    let obj = document.getElementById(id);
+    let range = end - start;
+    let current = start;
+    let increment = range / (duration / 10);
+    let timer = setInterval(function () {
+      current += increment;
+      if (
+        (increment > 0 && current >= end) ||
+        (increment < 0 && current <= end)
+      ) {
+        clearInterval(timer);
+        current = end;
+      }
+      obj.textContent = Math.round(current) + "+";
+    }, 10);
+  }
+
+  let totalDuration = 2000; 
+  animateValue("happyClients", 0, 145, totalDuration);
+  animateValue("projectsContact", 0, 258, totalDuration);
+  animateValue("businessPlan", 0, 365, totalDuration);
+  animateValue("awardWinner", 0, 890, totalDuration);
 });
-
-
